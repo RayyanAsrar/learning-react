7e822d090819f5159073199eb158b2b27c43c130
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-08-06 21:02:49 +0500

    done with the convertor

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -1,17 +1,81 @@
-import { useState } from 'react'
-
+import InputBox from './components/InputBox';
 import './App.css'
-
+import useCurrencyHooks from './hooks/useCurrencyHooks';
+import { useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0)
+    const [amount, setAmount] = useState(0);
+    const [from, setFrom] = useState("usd");
+    const [to, setTo] = useState("usd");
+    const [conversionRate, setConversionRate] = useState(0);
+    const currencyInfo = useCurrencyHooks(from);
+    const currencyOptions = Object.keys(currencyInfo);
 
-  return (
-    <>
-     <h1 className="text-3xl font-bold underline ">
-    Hello world!
-    </h1>
-    </>
-  )
+    const swapCurrency = () => {
+        setFrom(to);
+        setTo(from);
+    }
+
+    const convert = () => {
+  if (currencyInfo[to]) {
+    setConversionRate((amount * currencyInfo[to]).toFixed(2));
+  }
+};
+
+    return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzT0aF36Bn9PBzzTWsy6oGAxqLozVZR8dMJA&s')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert();
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                onCurrencyChange={(currency) => setFrom(currency)} // FIXED
+                                currencyOptions={currencyOptions}
+                                selectCurrency={from}
+                                onAmountChange={setAmount}
+                            />
+
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swapCurrency}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={conversionRate}
+                                onCurrencyChange={(currency) => setTo(currency)} // FIXED
+                                amountDisabled
+                                currencyOptions={currencyOptions}
+                                selectCurrency={to}
+                            />
+
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                            Convert  {from} to {to}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+
 }
 
 export default App
--- currencyConvertor/src/hooks/useCurrencyHooks.js
+++ currencyConvertor/src/hooks/useCurrencyHooks.js
@@ -1,7 +1,16 @@
-import {  use, useEffect,useState} from "react";
+import { useEffect,useState} from "react";
 
-function useCurrencyHooks() {
+function useCurrencyHooks(currency) {
+    const [currencyData, setCurrencyData] = useState({});
     useEffect(() => {
-        fetch()
-    },[])
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((response) => response.json())
+        .then((data) => {
+             setCurrencyData(data[currency]);;
+            // console.log(data.usd.pkr);  
+        })
+        
+    },[currency])
+      return currencyData;
 }
+export default useCurrencyHooks;
